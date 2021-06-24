a24bc71e002c9ba906e8331b59d7d609428514f5
Kartavya Sharma <Sharmakartavya06@gmail.com>
2021-06-24 08:14:56 +0000

    wip: adding nodemailer service

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./src/routes/index');
-var formRouter = require('./src/FormHandler/form_submit')
+var formRouter = require('./src/FormHandler/form_submit');
+var emailRouter = require('./src/EmailService/email_service');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/handle-form', formRouter);
+app.use('/send-email', emailRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/FormHandler/form_submit.js
+++ src/FormHandler/form_submit.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios')
+
+async function sendMail(requestBody) {
+    try {
+        return await axios.post('/send-email', {
+            formData: requestBody
+        })
+    } catch (error) {
+        return error;
+    }
+}
 
 router.post('/', (req, res) => {
-    console.log(req.body);
-    res.send(req.body.password);
+    try {
+        sendMail(req.body);
+    } catch (error) {
+        res.status(400);
+    }
+    res.status(201);
 });
 
 module.exports = router;
