f550f48007e7f0458c3c658550aaa1e4e13149ea
Kartavya Sharma <sharmakartavya06@gmail.com>
2021-07-10 14:14:47 +0530

    Connexion server is now functional

---
+++ web/controllers/mail_controller.py
@@ -0,0 +1,9 @@
+"""
+This is the mail_controller module, it supports all mail operations
+for kartavyas
+"""
+
+from flask import make_response, abort
+
+def send_mail():
+    return "Just sent mail"
