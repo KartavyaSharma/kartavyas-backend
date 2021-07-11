9ea26103fb20914f4cbac315f8ba97eee89d7ccc
Kartavya Sharma <sharmakartavya06@gmail.com>
2021-07-11 16:43:07 +0530

    SendGrid integration test

--- web/controllers/mail_controller.py
+++ web/controllers/mail_controller.py
@@ -4,6 +4,25 @@ for kartavyas
 """
 
 from flask import make_response, abort
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
 
 def send_mail():
-    return "Just sent mail"
+    message = Mail(
+        from_email=os.environ.get('SENDER'),
+        to_emails=os.environ.get('TARGET'),
+        subject='Sending with Twilio SendGrid is Fun',
+        html_content='<strong>and easy to do anywhere, even with python.</strong>'
+    )
+
+    try:
+        sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+        response = sg.send(message)
+        print(response.status_code)
+        print(response.body)
+        print(response.headers)
+    except Exception as e:
+        print(e.message)
+
+    
