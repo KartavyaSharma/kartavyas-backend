df485a559eb116faef54755048eaceebd4059137
Kartavya Sharma <sharmakartavya06@gmail.com>
2021-07-12 16:51:48 +0530

    Fixed template variable name

--- web/sendgrid_mailers/templates/test.py
+++
@@ -1,5 +0,0 @@
-with open("client.html", "r") as f:
-    for line in f.readlines():
-        if "{target_name}" in line:
-            line = line.replace("{target_name}", "Baani")
-            print(line)
